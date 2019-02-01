78400c628784257499180788aa078cf8bd0aaffc
MikeKBrown <3650408@myuwc.ac.za>
2019-02-01 07:15:05 +0200

    Homework Thursday Push

--- Day_3_Exercises.R
+++ Day_3_Exercises.R
@@ -1,2 +1,6 @@
 laminaria <- read_csv("data/laminaria.csv")
 save(laminaria, file = "data/laminaria.RData")
+
+#ggsn adds north arrows and scale bars in kilometers to maps that are generated using ggplot2 or ggmap.
+# Scales graphically adds scale map data to map aesthetics
+#Scales also provides methods for determining automatic breaks and axis lables and legends
